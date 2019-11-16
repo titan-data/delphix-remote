8b2e6b3e0ea08781427d04d39e334b772e7a881b
Eric Schrock <Eric.Schrock@delphix.com>
2019-11-16 00:23:44 -0500

    publish engine

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 repositories {
@@ -9,7 +10,7 @@ repositories {
 }
 
 val jar by tasks.getting(Jar::class) {
-    archiveBaseName.set("engine-sdk")
+    archiveBaseName.set("delphix-sdk")
 }
 
 dependencies {
@@ -17,4 +18,31 @@ dependencies {
     compile(kotlin("reflect"))
     compile("org.json:json:20190722")
     compile("com.squareup.okhttp3:okhttp:4.2.2")
+}
+
+// Maven publishing configuration
+val mavenBucket = when(project.hasProperty("mavenBucket")) {
+    true -> project.property("mavenBucket")
+    false -> "titan-data-maven"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.titandata"
+            artifactId = "delphix-sdk"
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "titan"
+            url = uri("s3://$mavenBucket")
+            authentication {
+                create<AwsImAuthentication>("awsIm")
+            }
+        }
+    }
 }
